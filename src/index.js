32c6507daa849186baab8ab6da24c9acbf6ac6fb
Pawel Ochmann <pawelochmann@poczta.onet.pl>
2023-06-29 21:04:33 +0200

    Download directory with svg weather icons, need to add them to project, and connect with weather info

--- src/index.js
+++ src/index.js
@@ -5,9 +5,20 @@ const main = document.querySelector('main');
 const button = document.querySelector('button');
 
 async function loadWeather() {
-  const location = document.querySelector('input').value;
-  const weather = await getData.getWeather(location);
-  console.log(JSON.stringify(weather));
+  try {
+    const location = document.querySelector('input').value;
+    const weather = await getData.getWeather(location);
+    console.log(JSON.stringify(weather));
+
+    const locationHeader = document.createElement('h1');
+    locationHeader.classList.add('locationHeader');
+    locationHeader.textContent = weather.location.name;
+    main.appendChild(locationHeader);
+  } catch (err) {
+    const errorInfo = document.createElement('h1');
+    errorInfo.textContent = 'We are sorry, could not find location You are looking for. Please try again';
+    main.appendChild(errorInfo);
+  }
 }
 
 button.addEventListener('click', async (e) => {
